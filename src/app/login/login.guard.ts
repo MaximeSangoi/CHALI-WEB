bbaad2f5bff24fd11617f374cf360895edcd6ebb
Maxime Sangoi <maxime.sangoi@gmail.com>
2023-05-19 17:45:15 +0200

    now 16

--- src/app/login/login.guard.ts
+++ src/app/login/login.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 import { LoginService } from './login.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
     constructor(private loginService: LoginService, private router: Router) { }
 
     canActivate(
